e0b0eceaa3f86d67f0a6f4697876bfeda9fce1e2
Vianny Molero <vianny.mo@gmail.com>
2020-11-15 23:23:38 -0300

    agregar registrar tiempo

---
+++ src/app/_models/time.model.ts
@@ -0,0 +1,18 @@
+export class Time {
+
+    constructor(
+        public t11: string,
+        public t12: string,
+        public t13: string,
+        public t21: string,
+        public t22: string,
+        public t23: string,
+        public t31: string,
+        public t32: string,
+        public t33: string,
+        public t1file: string,
+        public t2file: string,
+        public t3file: string
+    ) { }
+
+}
